007f3fedefb7a0179e1d57b9c7b5c492aca41818
Rehancodecraft <rehan.codecraft@gmail.com>
2025-07-24 09:56:41 +0500

    Updated Next greater Number

--- Next Greater Letter/Solution.java
+++ Next Greater Letter/Solution.java
@@ -4,17 +4,22 @@ public class Solution {
         System.out.println(nextGreatestLetter(letters,'e'));
 
     }
-    public static char nextGreatestLetter(char[] letters, char target) {
+public char nextGreatestLetter(char[] letters, char target) {
+
         int start = 0;
-        int end = letters.length-1;
-        while(start<= end){
-            int mid = start + (end-start)/2;
-            if(target > letters[mid]){
+        int end = letters.length - 1;
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
                 start = mid + 1;
-            }else{
-                end = mid-1;
             }
         }
         return letters[start % letters.length];
     }
-}
+
